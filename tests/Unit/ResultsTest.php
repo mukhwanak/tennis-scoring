94ada67b50d1c15b32cf186c67725316b340d80e
Kennedy Maikuma Lingo <mukhwanak@gmail.com>
2022-05-06 15:28:54 +0300

    Install Pest

---
+++ tests/Unit/ResultsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * User: Kennedy Mukhwana Maikuma Lingo
+ * Date: 06/05/2022
+ * Time: 15:11
+ */
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Http\Services\ResultsFinal;
+
+class ResultsTest extends TestCase
+{
+
+
+
+}
